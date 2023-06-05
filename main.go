9d3c999fb605c81e2d052fec15ccdda51b95d269
genkovich <genkovich@syneforge.com>
2023-06-05 17:13:08 +0300

    :zebra: add zookeeper

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import "course/biography"
+import "course/zoo"
 
 func main() {
-	b := biography.NewBiography("Кирило", "Сулімовський").
-		SetIsMarried(true).
-		SetDateOfBirth("1991-10-21").
-		SetChildren(2).
-		SetMajor("Computer Science").
-		AddPhoneNumber("+380931234567").
-		AddPhoneNumber("+380931234568").
-		AddPhoneNumber("+380931234569")
-
-	b.PrintBiography()
+	zoo.CatchAnimals()
 }
